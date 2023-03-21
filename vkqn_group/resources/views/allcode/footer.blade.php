e0f688318c9ede947c540d4c569ec2951d8b6206
quang <89928421+nguyendaiquang@users.noreply.github.com>
2023-03-21 10:13:19 +0700

    lan1

---
+++ vkqn_group/resources/views/allcode/footer.blade.php
@@ -0,0 +1,50 @@
+<div class="footer_container">
+                    <div class="footer_link footer_item">
+                        <h3 class="footer_heading ">LIÊN KẾT</h3>
+                        <ul>
+                            <li><a href="./index.php">Trang chủ</a></li>
+                            <li><a href="./dienthoai.php">Điện thoại</a></li>
+                            <li><a href="./laptop.php">Laptop</a></li>
+                            <li><a href="./tablet.php">Máy tính bảng</a></li>
+                            <li><a href="./phukien.php">Phụ kiện</a></li>
+                            <li><a href="./contact.php">Liên hệ</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer_info footer_item">
+                        <h3 class="footer_heading ">VỀ VKQN_Group</h3>
+                        <ul>
+                            <li><a href="./gtdreamteam/gtdreamteam.html">Giới thiệu VKQN_GROUP</a></li>
+                            <li><a href="">Tuyển dụng</a></li>
+                            <li><a href="">Điều khoản</a></li>
+                            <li><a href="">Chính sách bảo mật</a></li>
+                            <li><a href="">Tiếp thị liên kết cùng VKQN</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer_support footer_item">
+                        <h3 class="footer_heading">CHĂM SÓC KHÁCH HÀNG</h3>
+                        <ul>
+                            <li><a href="/csgh_thanhtoan">Giao hàng và thanh toán</a></li>
+                            <li><a href="/csbh">Chính sách bảo hành</a></li>
+                            <li><a href="/csdt">Chính sách đổi trả</a></li>
+                            <li><a href="/csdh">Hướng dẫn đặt hàng</a></li>
+                            <li><a href="/csbanhang">Hướng dẫn bán hàng</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer_contact footer_item">
+                        <h3 class="footer_heading">THÔNG TIN LIÊN HỆ</h3>
+                        <ul>
+                            <li><i class="fa fa-map-marker"></i> 147D, Tân Lập 2, phường Hiệp Phú, quận 9, TP Thủ Đức, TP HCM</li>
+                            <li><i class="fa fa-phone"></i><a href="tel:0123456789"> 0123 456 789</a></li>
+                            <li><i class="fa fa-envelope"></i><a href="mailto:dreamteam@gmail.com"> vkqn@gmail.com</a></li>
+                            <li><i class="fab fa-facebook"></i><a href=""> Facebook</a></li>
+                            <li><i class="fab fa-instagram"></i><a href=""> Instagram</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer_copyright">Copyright © 2022 Bản quyền thuộc về VKQN_GROUP
+                   
+                </div>
